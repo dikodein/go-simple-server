3b4d0ad4c528e4f42b1a4c87d0ab7faaddd9cb30
Achmad Chun-Chun Winata Adi <anonychun@gmail.com>
2020-08-21 22:34:35 +0700

    Adding middleware

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", index())
+	mux.HandleFunc("/", middleware(index()))
 
 	http.ListenAndServe(":8080", mux)
 }
 
 func index() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("I'm Awake"))
+		w.Write([]byte("I'm Awake\n"))
+	}
+}
+
+func middleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("You'l see me first\n"))
+		next.ServeHTTP(w, r)
 	}
 }
